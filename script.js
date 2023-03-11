230ee9a8b2b73f27113ee1ca8e259f68873b9942
thomaskrut <thomas.krut@yh.nackademin.se>
2023-03-11 22:02:30 +0100

    populate table function prototype

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ function populateTable(products) {
 
     const productsTable = document.querySelector('#products-table')
 
-products.forEach(p => {
-    const newTableRow = document.createElement("tr");
-    
-    const newTableCell = document.createElement("td");
-    newTableCell.textContent = p.title;
-    productsTable.appendChild(newTableRow);
-    newTableRow.appendChild(newTableCell);
-
-});
+    products.forEach(p => {
+        const newTableRow = document.createElement("tr");
+        const titleCell = document.createElement("td");
+        const productImage = document.createElement("img");
+        productImage.src = p.image;
+        titleCell.textContent = p.title;
+        titleCell.appendChild(productImage);
+        productsTable.appendChild(newTableRow);
+        newTableRow.appendChild(titleCell);
+    });
 
 }
 
