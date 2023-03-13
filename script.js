fca0bb79ce352628a2cfd34f580b88fecea42e36
thomaskrut <thomas.krut@yh.nackademin.se>
2023-03-13 09:32:22 +0100

    all product info + buttons added

--- script.js
+++ script.js
@@ -4,19 +4,60 @@ async function loadAllProducts() {
     populateTable(products);
 }
 
+function buyProduct(product) {
+    
+    // anting spara i localStorage och ladda order.html
+    // eller ladda order.html?productid=
+    
+    console.log(product);
+}
+
 function populateTable(products) {
 
     const productsTable = document.querySelector('#products-table')
 
     products.forEach(p => {
         const newTableRow = document.createElement("tr");
-        const titleCell = document.createElement("td");
+
+        const imageCell = document.createElement("td");
+
+        const descriptionCell = document.createElement("td");
+
         const productImage = document.createElement("img");
+
+        const title = document.createElement("p");
+        title.className = "title";
+        title.textContent = p.title;
+        descriptionCell.appendChild(title);
+
+        const category = document.createElement("p");
+        category.className = "category";
+        category.textContent = p.category;
+        descriptionCell.appendChild(category);
+
+        const description = document.createElement("p");
+        description.className = "description";
+        description.textContent = p.description;
+        descriptionCell.appendChild(description);
+        
+        const price = document.createElement("p");
+        price.className = "price";
+        price.textContent = "$"+ p.price.toFixed(2);
+        descriptionCell.appendChild(price);
+        
+        const buyButton = document.createElement("button");
+        buyButton.className = "btn btn-success";
+        buyButton.textContent = "Buy";
+        descriptionCell.appendChild(buyButton);
+        buyButton.addEventListener("mousedown", () => buyProduct(p));
+        
         productImage.src = p.image;
-        titleCell.textContent = p.title;
-        titleCell.appendChild(productImage);
+        imageCell.appendChild(productImage);
+        
+        
         productsTable.appendChild(newTableRow);
-        newTableRow.appendChild(titleCell);
+        newTableRow.appendChild(imageCell);
+        newTableRow.appendChild(descriptionCell);
     });
 
 }
